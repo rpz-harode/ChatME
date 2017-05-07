c66ecd9f1e4365af4298c0cee7126b68f608dd4b
rpz-harode <rupeshharode@gmail.com>
2017-05-08 00:01:57 +0530

    Added Bootstrap and Socket

--- server.js
+++ server.js
@@ -16,8 +16,11 @@ app.get('/',function(req, res){
 io.sockets.on('connection',function(socket)){
   //when a new user connects
   connection.push(socket);
-  console.log('Connected : %s sockets connected');
+  console.log('Connected : %s sockets connected',connections.length);
 
   //When someone disconnects
+  connection.splice(connections.indexOf(socket),1);
+  console.log('1 socket disconected : %s sockets connected' ,connections.length);
+  
 
 }
