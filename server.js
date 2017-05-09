ae09bf95e38ab4de6e720bbc8629c95334e10661
rpz-harode <rupeshharode@gmail.com>
2017-05-09 15:22:00 +0530

    Added login and improved chat

--- server.js
+++ server.js
@@ -20,8 +20,28 @@ io.sockets.on('connection',function(socket){
 
   //When someone disconnects
   socket.on('disconnect',function(data){
+    users.splice(users.indexOf(socket.username),1);
+    updateUsernames();
     connections.splice(connections.indexOf(socket),1);
     console.log('1 socket disconected : %s sockets connected' ,connections.length);
   });
 
+  //to implement message sending
+  socket.on('send message', function(data){
+      //console.log(data);
+      io.sockets.emit('new message' , {msg: data, user: socket.username});
+  });
+
+  //For new users
+  socket.on('new user' , function(data,callback){
+      callback(true);
+      socket.username = data;
+      users.push(socket.username);
+      updateUsernames();
+
+  });
+
+  function updateUsernames(){
+    io.sockets.emit('get users',users);
+  }
 });
